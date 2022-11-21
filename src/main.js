ce34829b18de0c5afa8cea35e6c46c97e0cd856b
Alexandre Sauner <alexandre.snr@icloud.com>
2022-11-20 22:46:42 -0800

    basic ping/pong bot

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-console.log('Hello, world');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, async (message) => {
+  if (message.author.id === client.user.id) {
+    return;
+  }
+
+  if (message.content === 'Ping') {
+    await message.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
